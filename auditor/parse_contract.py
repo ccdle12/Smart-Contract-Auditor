bf453da91d99684db0dbd6af2d16ff240ac5113f
ccdle12 <chris.coverdale24@gmail.com>
2018-03-21 14:33:23 +0000

    Created functions and tests for finding msg.sender.call and then finding the function containing the call

--- auditor/parse_contract.py
+++ auditor/parse_contract.py
@@ -2,11 +2,56 @@ import re
 
 class ContractParser: 
     def has_msg_sender(self, contract_as_string):
-        match_msg_sender = re.search(r'msg.sender', contract_as_string)
+        match_msg_sender = re.search(r'msg.sender.call', contract_as_string)
 
         if match_msg_sender != None:
-            print("Match Msg Sender: {}".format(match_msg_sender.group(0)))
             return True
 
         return False
     
+    def get_msg_sender_position(self, contract_as_string):
+        if not self.has_msg_sender(contract_as_string):
+            return None
+        
+        position = contract_as_string.find('msg.sender.call')
+
+        return position
+
+    def extract_withdraw_function(self, contract_as_string):
+        if not self.has_msg_sender(contract_as_string):
+            return None
+
+        # Get the position of the msg.sender.call        
+        msg_sender_position = self.get_msg_sender_position(contract_as_string)
+
+        # Get the last function call between the start of the contract to the msg.sender.call position
+        withdraw_function_position = contract_as_string.rfind('function', 0, msg_sender_position)
+
+        # Need to split a string according to function and if statements, need to take into account '}'
+        function_call_to_end_of_contract = "".join(contract_as_string[withdraw_function_position:].split())
+
+        # Temporary - Naive solution to find the end of the function
+        opening_brace = 0
+        closing_brace = None
+        last_position = None
+        i = withdraw_function_position
+
+        while opening_brace != closing_brace:
+            character = contract_as_string[i]
+
+            if character == '{':
+                opening_brace += 1
+            
+            if character == '}':
+                if closing_brace is None:
+                    closing_brace = 0
+
+                closing_brace += 1
+
+            last_position = i
+            i += 1
+
+        withdraw_function = contract_as_string[withdraw_function_position:last_position + 1]
+
+        return "".join(withdraw_function.split())
+        
--- auditor/tests.py
+++ auditor/tests.py
@@ -1,7 +1,7 @@
 import unittest
 import parse_contract as auditor
 
-class MainTest(unittest.TestCase):
+class TestContractParser(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -19,6 +19,9 @@ class MainTest(unittest.TestCase):
                               function withdraw() { WithdrawEvent(, balances[]); require(balances[] > 0); \
                               if (!call.value(balances[])()) { revert(); }   balances[] = 0; } }"
 
+        cls.withdraw_function_as_string = "".join("function withdraw() { WithdrawEvent(msg.sender, balances[msg.sender]); require(balances[msg.sender] > 0); \
+                                           if (!msg.sender.call.value(balances[msg.sender])()) { revert(); }   balances[msg.sender] = 0; }".split())
+
 
     def test_should_parse_string(self):                   
         print("[test_should_parse_string]")
@@ -28,11 +31,43 @@ class MainTest(unittest.TestCase):
         self.assertEqual(expected, has_msg_sender)
 
     def test_should_parse_string_2(self):                       
-        print("[test_should_parse_string]")
+        print("[test_should_parse_string_2]")
         print("Should return False that contract string does not have msg.sender\n")
         has_msg_sender = self.ContractParser.has_msg_sender(self.contract_as_string_fail)
         expected = False
         self.assertEqual(expected, has_msg_sender)
+
+    def test_should_get_position_of_msg_sender(self):
+        print('[test_should_get_position_of_msg_sender]')
+        print("Should return the position of msg.sender.call\n")
+
+        msg_sender_position = self.ContractParser.get_msg_sender_position(self.contract_as_string)
+        self.assertTrue(msg_sender_position is not None)
+    
+    def test_should_get_position_of_msg_sender_2(self):
+        print('[test_should_get_position_of_msg_sender_2]')
+        print("Should return None when searching for position of msg.sender.call\n")
+
+        msg_sender_position = self.ContractParser.get_msg_sender_position(self.contract_as_string_fail)
+        self.assertEqual(None, msg_sender_position)
+
+    def test_should_retrieve_withdraw_function(self):
+        print('[test_should_retrieve_withdraw_function]')
+        print("Should return the withdraw function given the position of the msg.sender.call\n")
+
+        extracted_withdraw_function = self.ContractParser.extract_withdraw_function(self.contract_as_string)
+        expected = self.withdraw_function_as_string
+
+        self.assertEqual(expected, extracted_withdraw_function)
+
+    def test_should_retrieve_withdraw_function_2(self):
+        print('[test_should_retrieve_withdraw_function_2]')
+        print("Should return None when extracting withdraw function is called given a contract without msg.sender.call\n")
+
+        extracted_withdraw_function = self.ContractParser.extract_withdraw_function(self.contract_as_string_fail)
+        expected = self.withdraw_function_as_string
+
+        self.assertEqual(None, extracted_withdraw_function)
 
 if __name__ == '__main__':
     unittest.main()
